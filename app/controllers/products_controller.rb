7c6398eda91fb8812b10162ca8bd639fc99efe4a
savka-olia <tokarchykolia@gmail.com>
2022-09-01 23:15:56 +0300

    add api key

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,13 @@
+require "json"
+require "rest-client"
+
 class ProductsController < ApplicationController
 
+	private
+
+	def get_api(query)
+		res = RestClient.get("https://api.calorieninjas.com/v1/nutrition?query=#{query}", headers={'X-Api-Key': ENV["CALORIE_API_KEY"]})
+		JSON.parse(res)
+	end
 end
   
